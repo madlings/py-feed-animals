e76d9dbf5165b8bc200a3f647281a093e0cbc7e1
madlings <nagalingam.s@gmail.com>
2026-02-13 13:35:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,40 @@
-#  write your code here
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+        self.name = name
+        self.appetite = appetite
+        self.is_hungry = is_hungry
+
+    def print_name(self) -> None:
+        print(f"Hello, I'm {self.name}")
+
+    def feed(self) -> int:
+        tmp = 0
+        if (self.is_hungry):
+            print(f"Eating {self.appetite} food points...")
+            self.is_hungry = False
+            tmp = self.appetite
+            self.appetite = 0
+        return tmp
+
+    @statitcmethod
+    def feed_animals(animals: list["Animal"]) -> int:
+        totalfeed = 0
+        for animal in animals:
+            totalfeed += animal.feed()
+        return totalfeed
+
+
+class Cat(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+        super().__init__(name, 3, is_hungry)
+
+    def catch_mouse(self) -> None:
+        print(f"The hunt began!")
+
+
+class Dog(Animal):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
+          super().__init__(name, 7, is_hungry)
+
+    def bring_slippers(self) -> None:
+          print(f"The slippers delivered!")
